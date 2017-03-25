07525d4d4c13e354c2bd2ef59bca0eecfa1b8694
matheustanaques <matheustanaques@gmail.com>
2017-03-25 14:29:21 -0300

    botao sala D

--- UniforLabs/app/src/main/java/br/unifor/dispmoveis/uniforlabs/MainActivity.java
+++ UniforLabs/app/src/main/java/br/unifor/dispmoveis/uniforlabs/MainActivity.java
@@ -1,13 +1,31 @@
 package br.unifor.dispmoveis.uniforlabs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btnD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnD = (Button) findViewById(R.id.btnD);
+        btnD.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+
+            case R.id.btnD:
+                Intent it = new Intent(this, SituacaoSala.class);
+        }
+
     }
 }
